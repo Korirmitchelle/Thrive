34c1cf649f63bc3016d1ac1ca9c42d081620d7b5
Mitchelle <korirmitchelle@gmail.com>
2019-10-05 19:14:17 +0300

    Add Collection View To Display Buttons On Home Page
    Added collection view and it's delegate methods, grouped files in different folders and added new icons.

--- thrive/HomeViewController.swift
+++ thrive/HomeViewController.swift
@@ -8,10 +8,42 @@
 
 import UIKit
 class HomeViewController: UIViewController {
-
+    var cells = 5
+    let buttonImageNames = ["ic_refresh","ic_cancel","ic_star","ic_like","ic_thunder"]
+    @IBOutlet weak var buttonsCollectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        buttonsCollectionView.delegate = self
+        buttonsCollectionView.dataSource = self
     }
+}
+extension HomeViewController:UICollectionViewDelegate,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cells
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "buttonscell", for: indexPath) as? ButtonsCollectionViewCell
+        cell?.buttonImageView.image = UIImage(named: buttonImageNames[indexPath.row])
+        return cell!
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        var insets = self.buttonsCollectionView.contentInset
+        let frameWidth = self.view.frame.size.width
+        let  collectionViewWidth = (self.buttonsCollectionView.collectionViewLayout as! UICollectionViewFlowLayout).itemSize.width
+        var  leftInsets = (frameWidth - (collectionViewWidth * CGFloat(cells))) * 0.5 - (CGFloat(cells-1) * 5)
+        if leftInsets <= 0 {
+            leftInsets = 0
+        }
+        insets.left = leftInsets
+        return insets
+        
+    }
+    
+    
+    
 }
---
+++ thrive/Views/ButtonsCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ButtonsCollectionViewCell.swift
+//  thrive
+//
+//  Created by Mitch on 05/10/2019.
+//  Copyright © 2019 Mitch. All rights reserved.
+//
+
+import UIKit
+
+class ButtonsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var buttonImageView: UIImageView!
+    
+    
+}
