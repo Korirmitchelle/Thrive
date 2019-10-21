0e13038bea4bfc5fe0157dc9e17d202a1db79b85
Mitchelle <korirmitchelle@gmail.com>
2019-10-21 16:59:37 +0300

    Use CollectionView
    Removed tab bar and used collection view.
    
    Signed-off-by: Mitchelle <korirmitchelle@gmail.com>

---
+++ Common Utilities/ImageviewExtension.swift
@@ -0,0 +1,9 @@
+//
+//  ImageviewExtension.swift
+//  thrive
+//
+//  Created by Mitch on 08/10/2019.
+//  Copyright © 2019 Mitch. All rights reserved.
+//
+
+import Foundation
---
+++ thrive/Views/ItemsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsCollectionViewCell.swift
+//  thrive
+//
+//  Created by Mitch on 17/10/2019.
+//  Copyright © 2019 Mitch. All rights reserved.
+//
+
+import UIKit
+
+class ItemsCollectionViewCell: UICollectionViewCell {
+    
+}
